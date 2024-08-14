057eca19d8ffa825a954c663375f0fad92c0984d
Dian Tri Utami <dian.it09@gmail.com>
2024-08-14 11:10:50 +0700

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,6 @@
 <!-- resources/views/auth/register.blade.php -->
-<!--extends('layouts.app')-->
-@extends ('template')
+@extends('layouts.app')
+<!--@extends ('template')-->
 @section('content')
   <div class="container">
     <h2>Register</h2>
