1b093d25aa80cd118ba7b5f375e8306ece622060
Dian Tri Utami <dian.it09@gmail.com>
2024-08-14 11:10:15 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,6 @@
 <!-- resources/views/auth/login.blade.php -->
-<!--extends('layouts.app')-->
-@extends('template')
+@extends('layouts.app')
+<!--extends('template')-->
 @section('content')
   <div class="container">
     <h2>Login</h2>
